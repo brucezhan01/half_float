0f7dfbd34145fb36d3b343fabd5c15db3beb7664
U-NVIDIA.COM\bzhan <bzhan@BZHAN-LT2.nvidia.com>
2018-02-05 17:42:20 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "half.hpp"
+
+using half_float::half;
+using half_float::half_cast;
+
+#define LENGTH    16
+int main()
+{
+    float a[LENGTH];
+    float b[LENGTH];
+    float c[LENGTH];
+    
+    half ha[LENGTH];
+    half hb[LENGTH];
+    half hc[LENGTH];   
+    
+    for (int i = 0; i < LENGTH; ++i) {
+        float r = static_cast<float>(rand())/static_cast<float>(RAND_MAX)*2.0f - 1.0f; // range: [-1, 1]
+        a[i] = r;
+        r = static_cast<float>(rand())/static_cast<float>(RAND_MAX)*2.0f - 1.0f;
+        b[i] = r;
+        c[i] = a[i]*b[i];
+        
+        ha[i] = half_cast<half, std::round_to_nearest>(a[i]);
+        hb[i] = half_cast<half, std::round_to_nearest>(b[i]);
+        hc[i] = ha[i]*hb[i];
+        
+        printf("%d:diff=%.8f, a=%.8f, b=%.8f, c=%.8f, ha=%.8f, hb=%.8f, hc=%.8f, hc_bin=0x%08x\n", i,fabs(c[i]-hc[i]),a[i],b[i],c[i],ha[i],hb[i],hc[i],hc[i].get_binary());
+    }
+    
+    return 0;
+}
